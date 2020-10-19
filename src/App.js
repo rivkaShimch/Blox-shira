3b1b0412b6f5d51aee0baa1496844ebb061fd04d
shira <shira@leader.codes>
2020-10-19 11:49:57 +0300

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 
+
+//commit to git
+
 // import NavBar from "./components/navbar";
 // import CreateUser from "./components/create-user";
 // import About from "./components/about";

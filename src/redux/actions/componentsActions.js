31ced28cd40524161451c201c62506cb45eed4a5
shira <shira@leader.codes>
2020-10-19 13:07:33 +0300

    add redux
    Co-Authored-By: rivkaShimch <33696268+rivkashimch@users.noreply.github.com>

---
+++ src/redux/actions/componentsActions.js
@@ -0,0 +1,7 @@
+export function setDisplayTitleEditor(title) {
+
+    return {
+        type: 'DISPLAY_TITLE_EDITOR',
+        payload: title
+    };
+}
--- src/redux/reducers/CanvasDetailsStore.js
+++ src/redux/reducers/CanvasDetailsStore.js
@@ -1,11 +1,17 @@
 
-import { OPERATE } from '../actions/canvasActions';
+// import { OPERATE } from '../actions/canvasActions';
+import { OPERATE } from '../actions/componentsActions';
+
 import produce from 'immer'
 
 const initialState = {
   canvasDetails: {
 
-    name: "ll"
+    name: "ll",
+    title: "title01"
+  },
+  displayComponents: {
+    display_title_editor: false
   }
 
 };
@@ -15,6 +21,12 @@ export default produce((state, action) => {
 
     case 'NAME_CANVAS':
       state.canvasDetails.name = action.payload;
+      break;
+    case 'TITLE_CANVAS':
+      state.canvasDetails.title = action.payload;
+      break;
+    case 'DISPLAY_TITLE_EDITOR':
+      state.displayComponents.display_title_editor = action.payload;
       break;
     default:
       return state;
